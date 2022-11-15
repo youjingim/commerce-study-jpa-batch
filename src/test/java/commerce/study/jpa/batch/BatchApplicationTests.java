6bd2a376f5952715a016357b3651cccee17d41c9
DESKTOP-RSA3TGI\yjkim <uiui3223@gmail.com>
2022-11-15 22:03:26 +0900

    Initial commit

---
+++ src/test/java/commerce/study/jpa/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package commerce.study.jpa.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
